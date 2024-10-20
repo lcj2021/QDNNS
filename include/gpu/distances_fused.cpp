7ae00b88c61bd410047a7f4dc3beb9d7e0909ac0
lcj2021 <channinglcj@gmail.com>
2024-10-20 17:38:39 +0800

    [refactor] remove hamming*

--- include/gpu/distances_fused.cpp
+++ include/gpu/distances_fused.cpp
@@ -1,42 +1,42 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+// /**
+//  * Copyright (c) Facebook, Inc. and its affiliates.
+//  *
+//  * This source code is licensed under the MIT license found in the
+//  * LICENSE file in the root directory of this source tree.
+//  */
 
-#include <distances_fused.h>
+// #include <distances_fused.h>
 
-#include <platform_macros.h>
+// #include <platform_macros.h>
 
-#include <avx512.h>
-#include <simdlib_based.h>
+// #include <avx512.h>
+// #include <simdlib_based.h>
 
-namespace faiss {
+// namespace faiss {
 
-bool exhaustive_L2sqr_fused_cmax(
-        const float* x,
-        const float* y,
-        size_t d,
-        size_t nx,
-        size_t ny,
-        Top1BlockResultHandler<CMax<float, int64_t>>& res,
-        const float* y_norms) {
-    if (nx == 0 || ny == 0) {
-        // nothing to do
-        return true;
-    }
+// bool exhaustive_L2sqr_fused_cmax(
+//         const float* x,
+//         const float* y,
+//         size_t d,
+//         size_t nx,
+//         size_t ny,
+//         Top1BlockResultHandler<CMax<float, int64_t>>& res,
+//         const float* y_norms) {
+//     if (nx == 0 || ny == 0) {
+//         // nothing to do
+//         return true;
+//     }
 
-#ifdef __AVX512F__
-    // avx512 kernel
-    return exhaustive_L2sqr_fused_cmax_AVX512(x, y, d, nx, ny, res, y_norms);
-#elif defined(__AVX2__) || defined(__aarch64__)
-    // avx2 or arm neon kernel
-    return exhaustive_L2sqr_fused_cmax_simdlib(x, y, d, nx, ny, res, y_norms);
-#else
-    // not supported, please use a general-purpose kernel
-    return false;
-#endif
-}
+// #ifdef __AVX512F__
+//     // avx512 kernel
+//     return exhaustive_L2sqr_fused_cmax_AVX512(x, y, d, nx, ny, res, y_norms);
+// #elif defined(__AVX2__) || defined(__aarch64__)
+//     // avx2 or arm neon kernel
+//     return exhaustive_L2sqr_fused_cmax_simdlib(x, y, d, nx, ny, res, y_norms);
+// #else
+//     // not supported, please use a general-purpose kernel
+//     return false;
+// #endif
+// }
 
-} // namespace faiss
+// } // namespace faiss
--- include/gpu/simdlib.h
+++ include/gpu/simdlib.h
@@ -7,36 +7,8 @@
 
 #pragma once
 
-/** Abstractions for 256-bit registers
- *
- * The objective is to separate the different interpretations of the same
- * registers (as a vector of uint8, uint16 or uint32), to provide printing
- * functions.
- */
-
-#if defined(__AVX512F__)
-
-#include <simdlib_avx2.h>
-#include <simdlib_avx512.h>
-
-#elif defined(__AVX2__)
-
-#include <simdlib_avx2.h>
-
-#elif defined(__aarch64__)
-
-#include <simdlib_neon.h>
-
-#elif defined(__PPC64__)
-
-#include <simdlib_ppc64.h>
-
-#else
-
 // emulated = all operations are implemented as scalars
 #include <simdlib_emulated.h>
 
 // FIXME: make a SSE version
 // is this ever going to happen? We will probably rather implement AVX512
-
-#endif
